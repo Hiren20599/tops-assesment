f72e41de41e411c1660e3cae18c168e57300aeea
Hiren J Patel <127721115+Hiren20599@users.noreply.github.com>
2023-04-05 11:43:42 +0530

    Add files via upload

---
+++ area circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float a,area;
+	printf("enter the radius of circle:- ");
+	scanf("%f",&a);
+	area=3.1428*a*a;
+	printf("\nthe area of circle is= %f",area);
+	return 0;
+}
---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,area;
+	printf("enter the length of rectangle:- ");
+	scanf("%d",&a);
+	printf("enter the width of rectangle:- ");
+	scanf("%d",&b);
+	area=a*b;
+	printf("\nthe area of rectangle is= %d",area);
+	return 0;
+}
